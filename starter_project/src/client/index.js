c19b64a46f9c9840646e5d76dce20d3b757d300c
Muayid Alhinai <muayid.ocmt@gmail.com>
2025-05-09 20:51:17 +0400

    testing

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -3,7 +3,12 @@ import { handleSubmit } from './js/formHandler'
 
 
 
-// alert("I EXIST")
+alert("I EXIST")
 // console.log("CHANGE!!");
 
 // sass files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
--- starter_project/webpack.prod.js
+++ starter_project/webpack.prod.js
@@ -2,6 +2,9 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -15,16 +18,21 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
         }
         ]
     },
+    optimization: {
+        minimizer: [
+            new TerserPlugin(), 
+            new CssMinimizerPlugin(),
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
         new WorkboxPlugin.GenerateSW()
+		new MiniCssExtractPlugin()
     ],
     devServer: {
         port: 3000,
